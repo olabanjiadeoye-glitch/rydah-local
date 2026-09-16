0038a31bb2fcdcd0d0a1a8d117422c22efbd055c
olabanjiadeoye-glitch <olabanjiadeoye@gmail.com>
2026-09-16 16:48:45 +0100

    Show animated Rydah splash on app load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionToolbar from "./session-toolbar";
+import RydahSplash from "./rydah-splash";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <RydahSplash />
         {children}
         <SessionToolbar />
       </body>
