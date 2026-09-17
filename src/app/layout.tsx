d1e03a1ae2b3acfaa311f99ce5f3de2e2715dd5e
olabanjiadeoye-glitch <olabanjiadeoye@gmail.com>
2026-09-17 10:37:08 +0100

    Keep fixed session toolbar from covering page actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col pb-24 md:pb-28">
         <RydahSplash />
         {children}
         <SessionToolbar />
