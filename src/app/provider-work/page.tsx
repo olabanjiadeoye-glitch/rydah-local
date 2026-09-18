754e16094f339d275c76a6477852d1c487f5d18b
olabanjiadeoye-glitch <olabanjiadeoye@gmail.com>
2026-09-18 07:17:48 +0100

    Add fresh provider work dashboard route

---
+++ src/app/provider-work/page.tsx
@@ -0,0 +1,8 @@
+import ProviderDashboardPage from "../provider-dashboard/page";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function ProviderWorkPage() {
+  return <ProviderDashboardPage />;
+}
